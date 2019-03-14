34ec742d561ace62dc2da96206c2076b41a1c87a
Nikita Ivanov <nikita-ivanov.mail@yandex.ru>
2019-03-15 01:12:50 +0300

    Refactor rope gun a bit

--- Assets/src/Aster/Objects/Rope/RopeGun.cs
+++ Assets/src/Aster/Objects/Rope/RopeGun.cs
@@ -7,21 +7,21 @@ namespace Aster.Objects.Rope {
 [RequireComponent(typeof(LineRenderer))]
 public class RopeGun: MonoBehaviour
 {
-    [Range(1f, 50f)]
-    public float ShootForce = 10f;
+    [Range(.1f, 5f)]
+    public float ShootForce = 1f;
 
     public GameObject RopeSegmentPrefab;
     public GameObject RopeProjectilePrefab;
-    public Vector3 ProjectileSpawnOffset;
+    public Transform BulletSpawn;
 
     public Transform RopeObject;
 
     [Range(2, 50)]
     public int MaxRopeSegments;
 
-    public List<SpringJoint> RopeJoints;
-    public List<Transform> RopeTransforms;
-    public Rigidbody lastSpawned = null;
+    private int segmentsSpawned = 0;
+    private List<Transform> RopeTransforms;
+    private Rigidbody lastSpawned = null;
 
     private SpringJoint selfJoint;
     private LineRenderer ropeRenderer;
@@ -30,19 +30,21 @@ public class RopeGun: MonoBehaviour
     {
         selfJoint = GetComponent<SpringJoint>();
         ropeRenderer = GetComponent<LineRenderer>();
+        RopeTransforms = new List<Transform>(MaxRopeSegments);
     }
 
     public void Update()
     {
         // update line renderer points
-        if (ropeRenderer.positionCount != RopeTransforms.Count)
+        if (ropeRenderer.positionCount != RopeTransforms.Count + 1)
         {
-            ropeRenderer.positionCount = RopeTransforms.Count;
+            ropeRenderer.positionCount = RopeTransforms.Count + 1;
         }
         for (int i = 0; i < RopeTransforms.Count; i++)
         {
             ropeRenderer.SetPosition(i, RopeTransforms[i].position);
         }
+        ropeRenderer.SetPosition(RopeTransforms.Count, transform.position);
 
         if (lastSpawned != null) return;
 
@@ -66,8 +68,8 @@ public class RopeGun: MonoBehaviour
     {
         var projectile = Instantiate(
             RopeProjectilePrefab,
-            transform.position + ProjectileSpawnOffset,
-            Quaternion.identity,
+            BulletSpawn.position,
+            Quaternion.LookRotation(BulletSpawn.forward, BulletSpawn.up),
             RopeObject
         );
         lastSpawned = projectile.GetComponent<Rigidbody>();
@@ -80,7 +82,7 @@ public class RopeGun: MonoBehaviour
 
     public void SpawnRopeSegment()
     {
-        if (RopeJoints.Count >= MaxRopeSegments)
+        if (segmentsSpawned >= MaxRopeSegments)
         {
             Debug.Log("Can't spawn more segments, limit exceeded");
 
@@ -92,7 +94,7 @@ public class RopeGun: MonoBehaviour
         var ropeSegment = Instantiate(RopeSegmentPrefab, transform.position, Quaternion.identity, RopeObject);
 
         var joint = ropeSegment.GetComponent<SpringJoint>();
-        RopeJoints.Add(joint);
+        segmentsSpawned += 1;
         RopeTransforms.Add(ropeSegment.transform);
 
         joint.connectedBody = lastSpawned;
