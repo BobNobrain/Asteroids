1d7094ab90da03db20458d2e2107ab0c4df78bd0
Nikita Ivanov <nikita-ivanov.mail@yandex.ru>
2019-08-11 16:21:47 +0300

    Add gatherer interface
    Implemented raycast-based item detection and displaying of its name in UI

---
+++ Assets/src/Aster/Player/PlayerGatherer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Aster.Objects;
+
+public class PlayerGatherer: MonoBehaviour
+{
+    public Text crosshairLabel;
+    private int gatherableItemsMask;
+    public Transform raycaster;
+    public float maxGatherDistance;
+
+    private void Awake()
+    {
+        gatherableItemsMask = LayerMask.NameToLayer("Gatherables");
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(raycaster.position, raycaster.forward, out hit, maxGatherDistance, 1 << gatherableItemsMask))
+        {
+            var g = hit.transform.GetComponent<GatherableObject>();
+            if (g != null && g.gatherableItem != null)
+            {
+                crosshairLabel.text = g.gatherableItem.type.name;
+                return;
+            }
+        }
+
+        crosshairLabel.text = "";
+    }
+}
